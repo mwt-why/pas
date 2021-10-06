f9c1d06b6e9532f3b76a8e4ea7e2b16d90f6d8e4
WHY <1757751022@qq.com>
2021-10-06 11:52:15 +0800

    start ty

--- script/base_script.py
+++ script/base_script.py
@@ -5,4 +5,10 @@ from abc import ABC, abstractmethod
 class BaseScript(ABC):
     @abstractmethod
     def start():
-        LOG.info("start script")
+        LOG.warn("this is a abtract method,please implement it")
+
+    def run(self):
+        method_name = start()
+        while(True):
+            method = getattr(self, method_name)
+            method_na = method()
---
+++ script/ty.py
@@ -0,0 +1,10 @@
+from base_script import BaseScript
+
+
+class Ty(BaseScript):
+    def start():
+
+        return 'end'
+
+    def end():
+        print('i am over!')
