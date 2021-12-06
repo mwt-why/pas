18ca8d4768a9a368d2eb903ab8f1f426f3dd9720
WHY <1757751022@qq.com>
2021-12-06 16:47:36 +0800

    手机扫描工具集成

--- phone/phone_util.py
+++ phone/phone_util.py
@@ -1,9 +1,32 @@
 import uiautomator2 as u2
-import cv2
+from phone import nmap_util
 
-image_path = '/home/why/dataset/cur_image/cur_image.jpg'
 
-d = u2.connect('emulator-5554')
-# d = u2.connect_wifi('10.0.2.16')
-image = d.screenshot(format='opencv')
-cv2.imwrite(image_path, image)
+def test_connect_wifi(ip):
+    try:
+        u2.connect_wifi(ip)
+    except u2.exceptions.ConnectError:
+        return False
+    else:
+        return True
+
+
+def test_connect_serial(serial):
+    try:
+        u2.connect(serial)
+    except u2.exceptions.ConnectError:
+        return False
+    else:
+        return True
+
+
+def list_phone_ip():
+    ip_list = []
+    ips = nmap_util.scan_device_ip()
+    for ip in ips:
+        if test_connect_wifi(ip):
+            ip_list.append(ip)
+
+
+def list_phone_ip_test():
+    return ['192.168.43.23', '192.168.55']
